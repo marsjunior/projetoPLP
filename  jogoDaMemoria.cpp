4dbd8364447a23d81db677eaf5acd1f6e47504ff
MARCOS ANTONIO BATISTA DE OLIVEIRA JUNIO 116110176 <marcosaboj@lcc2-17.lcc.ufcg.edu.br>
2018-05-18 16:02:42 -0300

    Assinatura de Métodos

---
+++  jogoDaMemoria.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int particionaEntrada(string entrada){
+}
+
+bool verificaPares(int vetor1, int vetor2){
+}
+
+void modificarMatriz(){
+}
+
+string matrizController(){
+}
+
+void resetMatriz(){
+}
+
+int main(){
+    string entrada1, entrada2;
+    int nivel;
+    //int matrizModelo[5][5] =;
+    cin >> nivel;
+    cin >> entrada1;
+    cin >> entrada2;
+    particionaEntrada(entrada1);
+    particionaEntrada(entrada2);
+
+    return 0;
+}
