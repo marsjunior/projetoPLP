affd297fc89e517d8f3b4369de4a058fbffc0f16
MARCOS ANTONIO BATISTA DE OLIVEIRA JUNIO 116110176 <marcosaboj@lcc2-18.lcc.ufcg.edu.br>
2018-05-21 17:24:43 -0300

    Implementação de alguns métodos

---  jogoDaMemoria.cpp
+++  jogoDaMemoria.cpp
@@ -3,8 +3,6 @@
 
 using namespace std;
 
-int particionaEntrada(string entrada){
-}
 
 bool verificaPares(int vetor1, int vetor2){
 }
@@ -13,20 +11,37 @@ void modificarMatriz(){
 }
 
 string matrizController(){
+    string  matriz  = "     1 2 3 4 5 6\n";
+            matriz += "   1 * * * * * *\n";
+            matriz += "   2 * * * * * *\n";
+            matriz += "   3 * * * * * *\n";
+            matriz += "   4 * * * * * *\n";
+            matriz += "   5 * * * * * *\n";
+            matriz += "   6 * * * * * *\n";
+    return matriz;
+}
+
+string limpaTela(){
+    string retorno = "";
+    for(int i = 0; i < 100; i++){
+        retorno += "\n";
+    }
+    return retorno;
 }
 
 void resetMatriz(){
 }
 
 int main(){
-    string entrada1, entrada2;
+    int entrada1[2], entrada2[2];
     int nivel;
-    //int matrizModelo[5][5] =;
-    cin >> nivel;
-    cin >> entrada1;
-    cin >> entrada2;
-    particionaEntrada(entrada1);
-    particionaEntrada(entrada2);
 
+
+    cin >> nivel;
+    cout << matrizController();
+    cin >> entrada1[0] >> entrada1[1];
+    cin >> entrada2[0] >> entrada2[1];
+
+    cout << entrada1[0] << entrada1[1] << entrada2[0] << entrada2[1];
     return 0;
 }
