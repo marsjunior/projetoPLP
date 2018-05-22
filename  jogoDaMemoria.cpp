57ef64f8319d5a1904d9d1621e1803c09edf6c45
marianaalucena <mariana.lucena@ccc.ufcg.edu.br>
2018-05-22 10:46:53 -0300

    Modificando o metodo matrizController

---  jogoDaMemoria.cpp
+++  jogoDaMemoria.cpp
@@ -11,14 +11,39 @@ void modificarMatriz(){
 }
 
 string matrizController(){
-    string  matriz  = "     1 2 3 4 5 6\n";
-            matriz += "   1 * * * * * *\n";
-            matriz += "   2 * * * * * *\n";
-            matriz += "   3 * * * * * *\n";
-            matriz += "   4 * * * * * *\n";
-            matriz += "   5 * * * * * *\n";
-            matriz += "   6 * * * * * *\n";
-    return matriz;
+    if(nivel == 1){
+       string  matriz  = "     1 2 3 4 \n";
+                matriz += "   1 * * * * \n";
+                matriz += "   2 * * * * \n";
+                matriz += "   3 * * * * \n";
+                matriz += "   4 * * * * \n";
+        return matriz;
+    }
+    else if(nivel == 2){
+        string  matriz  = "     1 2 3 4 5 6 \n";
+                matriz += "   1 * * * * * * \n";
+                matriz += "   2 * * * * * * \n";
+                matriz += "   3 * * * * * * \n";
+                matriz += "   4 * * * * * * \n";
+                matriz += "   5 * * * * * * \n";
+                matriz += "   6 * * * * * * \n";
+        return matriz;
+    }
+    else if(nivel == 3){
+        string  matriz  = "     1 2 3 4 5 6 7 8 \n";
+                matriz += "   1 * * * * * * * * \n";
+                matriz += "   2 * * * * * * * * \n";
+                matriz += "   3 * * * * * * * * \n";
+                matriz += "   4 * * * * * * * * \n";
+                matriz += "   5 * * * * * * * * \n";
+                matriz += "   6 * * * * * * * * \n";
+                matriz += "   7 * * * * * * * * \n";
+                matriz += "   8 * * * * * * * * \n";
+        return matriz;
+    }
+    else{
+        cout << "Insira um nivel valido"
+    }
 }
 
 string limpaTela(){
@@ -36,7 +61,7 @@ int main(){
     int entrada1[2], entrada2[2];
     int nivel;
 
-
+    cout << "Escolha um dos niveis:" << endl << "1. Iniciante" << endl << "2. Intermediario" << endl << "3. Avancado";
     cin >> nivel;
     cout << matrizController();
     cin >> entrada1[0] >> entrada1[1];
